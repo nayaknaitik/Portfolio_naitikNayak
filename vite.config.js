62db5721cb047fa3f25a918612a32d7018c38ddf
Naitik Nayak <naitiknayak009@gmail.com>
2025-02-24 22:55:53 +0530

    deplou

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base:"/Portfolio_naitikNayak"
+  base:"/Portfolio_naitikNayak/"
 })
